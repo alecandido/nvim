1172ece3fef260b4cae91d53afba62b793dba673
Alessandro Candido <candido.ale@gmail.com>
2023-08-23 19:54:51 +0200

    Reduce preview size, since undos are usually small

--- lua/plugins/telescope/builtin.lua
+++ lua/plugins/telescope/builtin.lua
@@ -59,7 +59,7 @@ M.opts = {
       side_by_side = true,
       layout_strategy = "vertical",
       layout_config = {
-        preview_height = 0.8,
+        preview_height = 0.6,
       },
     },
   },
