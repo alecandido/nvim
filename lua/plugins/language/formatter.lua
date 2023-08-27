6ecf8fc3fd28b1ba88599b6ec472291fc17a6669
Alessandro Candido <candido.ale@gmail.com>
2023-08-27 10:23:06 +0200

    Configure most relevant formatters

--- lua/plugins/language/formatter.lua
+++ lua/plugins/language/formatter.lua
@@ -10,19 +10,48 @@ M.init = function()
   })
 end
 
+local function fnlfmt()
+  return {
+    exe = "fnlfmt",
+    stdin = true,
+    args = {},
+  }
+end
+
 M.config = function(_, opts)
   opts.filetype = {
-    lua = { require("formatter.filetypes.lua").stylua },
-    nix = { require("formatter.filetypes.nix").alejandra },
-    fennel = {
-      function()
-        return {
-          exe = "fnlfmt",
-          stdin = true,
-          args = {},
-        }
-      end,
+    c = { require("formatter.filetypes.c").uncrustify },
+    cpp = { require("formatter.filetypes.cpp").uncrustify },
+    css = { require("formatter.filetypes.css").prettier },
+    fennel = { fnlfmt },
+    go = {
+      require("formatter.filetypes.go").gofmt,
+      require("formatter.filetypes.go").goimports,
     },
+    graphql = { require("formatter.filetypes.graphql").prettier },
+    haskell = { require("formatter.filetypes.haskell").stylish_haskell },
+    html = { require("formatter.filetypes.html").prettier },
+    javascript = { require("formatter.filetypes.javascript").prettier },
+    javascriptreact = { require("formatter.filetypes.javascriptreact").prettier },
+    json = { require("formatter.filetypes.json").prettier },
+    kotlin = { require("formatter.filetypes.kotlin").ktlint },
+    lua = { require("formatter.filetypes.lua").stylua },
+    markdown = { require("formatter.filetypes.markdown").prettier },
+    nix = { require("formatter.filetypes.nix").alejandra },
+    python = {
+      require("formatter.filetypes.python").black,
+      require("formatter.filetypes.python").isort,
+      require("formatter.filetypes.python").docformatter,
+    },
+    rust = { require("formatter.filetypes.rust").rustfmt },
+    sh = { require("formatter.filetypes.sh").shfmt },
+    sql = { require("formatter.filetypes.sql").pgformat },
+    svelte = { require("formatter.filetypes.svelte").prettier },
+    toml = { require("formatter.filetypes.toml").taplo },
+    typescript = { require("formatter.filetypes.typescript").prettier },
+    typescriptreact = { require("formatter.filetypes.typescriptreact").prettier },
+    vue = { require("formatter.filetypes.vue").prettier },
+    yaml = { require("formatter.filetypes.yaml").prettier },
 
     ["*"] = require("formatter.filetypes.any"),
   }
