48df303f792465b3af669ac9e6dd01da1f3e95d5
Alessandro Candido <candido.ale@gmail.com>
2023-08-29 12:15:20 +0200

    Specify config file for vale

--- lua/plugins/language/lint.lua
+++ lua/plugins/language/lint.lua
@@ -25,12 +25,28 @@ M.opts = {
   yaml = { "yamllint" },
 }
 
+local function get_cur_file_extension(bufnr)
+  bufnr = bufnr or 0
+  return "." .. vim.fn.fnamemodify(vim.api.nvim_buf_get_name(bufnr), ":e")
+end
+
 M.init = function()
   vim.api.nvim_create_autocmd({ "BufWritePost" }, {
     callback = function()
       require("lint").try_lint()
     end,
   })
+
+  local vale = require("lint.linters.vale")
+  vale.args = {
+    "--no-exit",
+    "--output",
+    "JSON",
+    "--ext",
+    get_cur_file_extension,
+    "--config",
+    "$XDG_CONFIG_HOME/vale/vale.ini",
+  }
 end
 
 M.event = "BufWritePost"
